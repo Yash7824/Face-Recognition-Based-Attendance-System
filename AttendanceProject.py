a9b556564a3e4ad9760869fb321cea7ea333ca27
Yash7824 <yashr7824@gmail.com>
2021-07-26 10:30:30 +0530

    Initial commit

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -15,16 +15,18 @@ for cl in myList:
     classnames.append(os.path.splitext(cl)[0])
 print(classnames)
 
+
 def findEncodings(images):
     encodeList = []
     for img in images:
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         encode = face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
 
+
 def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
+    with open("Attendance.csv", 'r+') as f:
         myDataList = f.readlines()
         nameList = []
         for line in myDataList:
@@ -35,6 +37,7 @@ def markAttendance(name):
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
 
+
 encodeListKnown = findEncodings(images)
 print('Encoding completed')
 
@@ -42,15 +45,15 @@ cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
-    imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
     facesCurFrame = face_recognition.face_locations(imgS)
     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    for encodeFace,faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
         # print(faceDis)
 
         matchIndex = np.argmin(faceDis)
@@ -58,17 +61,11 @@ while True:
         if matches[matchIndex]:
             name = classnames[matchIndex].upper()
             # print(name)
-            y1,x2,y2,x1 = faceLoc
-            y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0))
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            y1, x2, y2, x1 = faceLoc
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0))
+            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
             markAttendance(name)
-
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
-
-
-
-
-
